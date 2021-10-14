8b079cae8a637f6ad606f1ea6363b2e780b9b438
yangfan17 <173838362@qq.com>
2021-10-14 09:23:46 +0800

    fix header notification time format

--- angular/src/app/shared/layout/notifications/UserNotificationHelper.ts
+++ angular/src/app/shared/layout/notifications/UserNotificationHelper.ts
@@ -7,130 +7,130 @@ import { ModalHelper } from '@delon/theme';
 import { DateTimeService } from '@app/shared/common/timing/date-time.service';
 
 export interface IFormattedUserNotification {
-    userNotificationId: string;
-    text: string;
-    time: string;
-    creationTime: Date;
-    icon: string;
-    state: String;
-    data: any;
-    url: string;
-    isUnread: boolean;
+  userNotificationId: string;
+  text: string;
+  time: string;
+  creationTime: Date;
+  icon: string;
+  state: String;
+  data: any;
+  url: string;
+  isUnread: boolean;
 }
 
 @Injectable()
 export class UserNotificationHelper {
+  constructor(
+    private _modalHelper: ModalHelper,
+    private _notificationService: NotificationServiceProxy,
+    private _dateTimeService: DateTimeService
+  ) {}
 
-    constructor(
-        private _modalHelper: ModalHelper,
-        private _notificationService: NotificationServiceProxy,
-        private _dateTimeService: DateTimeService
-    ) {
+  getUrl(userNotification: abp.notifications.IUserNotification): string {
+    switch (userNotification.notification.notificationName) {
+      case 'App.NewUserRegistered':
+        return '/app/admin/users?filterText=' + userNotification.notification.data.properties.emailAddress;
+      case 'App.NewTenantRegistered':
+        return '/app/admin/tenants?filterText=' + userNotification.notification.data.properties.tenancyName;
+      case 'App.GdprDataPrepared':
+        return (
+          AppConsts.remoteServiceBaseUrl +
+          '/File/DownloadBinaryFile?id=' +
+          userNotification.notification.data.properties.binaryObjectId +
+          '&contentType=application/zip&fileName=collectedData.zip'
+        );
+      //Add your custom notification names to navigate to a URL when user clicks to a notification.
     }
 
-    getUrl(userNotification: abp.notifications.IUserNotification): string {
-        switch (userNotification.notification.notificationName) {
-            case 'App.NewUserRegistered':
-                return '/app/admin/users?filterText=' + userNotification.notification.data.properties.emailAddress;
-            case 'App.NewTenantRegistered':
-                return '/app/admin/tenants?filterText=' + userNotification.notification.data.properties.tenancyName;
-            case 'App.GdprDataPrepared':
-                return AppConsts.remoteServiceBaseUrl + '/File/DownloadBinaryFile?id=' + userNotification.notification.data.properties.binaryObjectId + '&contentType=application/zip&fileName=collectedData.zip';
-            //Add your custom notification names to navigate to a URL when user clicks to a notification.
+    //No url for this notification
+    return '';
+  }
+
+  /* PUBLIC functions ******************************************/
+
+  getUiIconBySeverity(severity: abp.notifications.severity): string {
+    switch (severity) {
+      case abp.notifications.severity.SUCCESS:
+        return 'check-circle-o';
+      case abp.notifications.severity.WARN:
+        return 'warning';
+      case abp.notifications.severity.ERROR:
+        return 'close-circle-o';
+      case abp.notifications.severity.FATAL:
+        return 'close-circle-o';
+      case abp.notifications.severity.INFO:
+      default:
+        return 'info-circle-o';
+    }
+  }
+
+  format(userNotification: abp.notifications.IUserNotification, truncateText?: boolean): IFormattedUserNotification {
+    let formatted: IFormattedUserNotification = {
+      userNotificationId: userNotification.id,
+      text: abp.notifications.getFormattedMessageFromUserNotification(userNotification),
+      time: this._dateTimeService.formatJSDate(userNotification.notification.creationTime, 'yyyy-LL-dd HH:mm:ss'),
+      creationTime: userNotification.notification.creationTime,
+      icon: this.getUiIconBySeverity(userNotification.notification.severity),
+      state: abp.notifications.getUserNotificationStateAsString(userNotification.state),
+      data: userNotification.notification.data,
+      url: this.getUrl(userNotification),
+      isUnread: userNotification.state === abp.notifications.userNotificationState.UNREAD
+    };
+
+    if (truncateText || truncateText === undefined) {
+      formatted.text = abp.utils.truncateStringWithPostfix(formatted.text, 100);
+    }
+
+    return formatted;
+  }
+
+  show(userNotification: abp.notifications.IUserNotification): void {
+    //Application notification
+    abp.notifications.showUiNotifyForUserNotification(userNotification, {
+      onclick: () => {
+        //Take action when user clicks to live toastr notification
+        let url = this.getUrl(userNotification);
+        if (url) {
+          location.href = url;
         }
+      }
+    });
 
-        //No url for this notification
-        return '';
-    }
-
-    /* PUBLIC functions ******************************************/
-
-    getUiIconBySeverity(severity: abp.notifications.severity): string {
-        switch (severity) {
-            case abp.notifications.severity.SUCCESS:
-                return 'check-circle-o';
-            case abp.notifications.severity.WARN:
-                return 'warning';
-            case abp.notifications.severity.ERROR:
-                return 'close-circle-o';
-            case abp.notifications.severity.FATAL:
-                return 'close-circle-o';
-            case abp.notifications.severity.INFO:
-            default:
-                return 'info-circle-o';
+    if (Push.default.Permission.has()) {
+      //Desktop notification
+      Push.default.create('AbpZeroTemplate', {
+        body: this.format(userNotification).text,
+        icon: abp.appPath + 'assets/common/images/app-logo-small.png',
+        timeout: 6000,
+        onClick: function () {
+          window.focus();
+          this.close();
         }
+      });
     }
+  }
 
-    format(userNotification: abp.notifications.IUserNotification, truncateText?: boolean): IFormattedUserNotification {
-        let formatted: IFormattedUserNotification = {
-            userNotificationId: userNotification.id,
-            text: abp.notifications.getFormattedMessageFromUserNotification(userNotification),
-            time: this._dateTimeService.formatJSDate(userNotification.notification.creationTime, 'yyyy-LL-dd HH:mm:ss'),
-            creationTime: userNotification.notification.creationTime,
-            icon: this.getUiIconBySeverity(userNotification.notification.severity),
-            state: abp.notifications.getUserNotificationStateAsString(userNotification.state),
-            data: userNotification.notification.data,
-            url: this.getUrl(userNotification),
-            isUnread: userNotification.state === abp.notifications.userNotificationState.UNREAD
-        };
+  setAllAsRead(callback?: () => void): void {
+    this._notificationService.setAllNotificationsAsRead().subscribe(() => {
+      abp.event.trigger('app.notifications.refresh');
+      if (callback) {
+        callback();
+      }
+    });
+  }
 
-        if (truncateText || truncateText === undefined) {
-            formatted.text = abp.utils.truncateStringWithPostfix(formatted.text, 100);
-        }
+  setAsRead(userNotificationId: string, callback?: (userNotificationId: string) => void): void {
+    const input = new EntityDtoOfGuid();
+    input.id = userNotificationId;
+    this._notificationService.setNotificationAsRead(input).subscribe(() => {
+      abp.event.trigger('app.notifications.read', userNotificationId);
+      if (callback) {
+        callback(userNotificationId);
+      }
+    });
+  }
 
-        return formatted;
-    }
-
-    show(userNotification: abp.notifications.IUserNotification): void {
-
-        //Application notification
-        abp.notifications.showUiNotifyForUserNotification(userNotification, {
-            'onclick': () => {
-                //Take action when user clicks to live toastr notification
-                let url = this.getUrl(userNotification);
-                if (url) {
-                    location.href = url;
-                }
-            }
-        });
-        
-        if (Push.default.Permission.has()) {
-            //Desktop notification
-            Push.default.create('AbpZeroTemplate', {
-                body: this.format(userNotification).text,
-                icon: abp.appPath + 'assets/common/images/app-logo-small.png',
-                timeout: 6000,
-                onClick: function () {
-                    window.focus();
-                    this.close();
-                }
-            });
-        }
-    }
-
-    setAllAsRead(callback?: () => void): void {
-        this._notificationService.setAllNotificationsAsRead().subscribe(() => {
-            abp.event.trigger('app.notifications.refresh');
-            if (callback) {
-                callback();
-            }
-        });
-    }
-
-    setAsRead(userNotificationId: string, callback?: (userNotificationId: string) => void): void {
-        const input = new EntityDtoOfGuid();
-        input.id = userNotificationId;
-        this._notificationService.setNotificationAsRead(input).subscribe(() => {
-            abp.event.trigger('app.notifications.read', userNotificationId);
-            if (callback) {
-                callback(userNotificationId);
-            }
-        });
-    }
-
-    openSettingsModal(): void {
-        this._modalHelper
-            .createStatic(NotificationSettingsModalComponent, {}, {size: 'md'})
-            .subscribe(() => { });
-    }
+  openSettingsModal(): void {
+    this._modalHelper.createStatic(NotificationSettingsModalComponent, {}, { size: 'md' }).subscribe(() => {});
+  }
 }
